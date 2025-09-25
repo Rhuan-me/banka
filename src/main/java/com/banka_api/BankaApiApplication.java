b3bfeb4163194281b2ab6ff133681a96dc876edf
Rhuan Miguel Esteves <laterhuanme@gmail.com>
2025-09-25 10:24:35 -0300

    RME/initalizeProject

---
+++ src/main/java/com/banka_api/BankaApiApplication.java
@@ -0,0 +1,13 @@
+package com.banka_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banka_api/BankaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banka_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
